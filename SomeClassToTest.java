35e591cd3dbae31ce4e69335e7bde75c899f0b51
Posse <79206111373@yandex.ru>
2021-02-08 15:26:45 +0300

    First_init_of_Java3.HW7

---
+++ SomeClassToTest.java
@@ -0,0 +1,14 @@
+public class SomeClassToTest {
+
+    public void ex1(){
+        System.out.println("Hello world!");
+    }
+
+    public String ex2 (int i){
+        return String.valueOf(i);
+    }
+
+    public void divide(int x, int y) throws ArithmeticException{
+        System.out.println(x/y);
+    }
+}
